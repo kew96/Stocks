695a993871e6748286e474efdf554bc7af7bad04
Kyle <kew96@cornell.edu>
2020-02-11 12:46:37 -0500

    Updating stock search, lets you pick if you miss the symbol/name

--- functions/stock_data.py
+++ functions/stock_data.py
@@ -1,5 +1,5 @@
 from alpha_vantage.timeseries import TimeSeries
-from .errors import *
+import errors
 
 alpha_vantage_KEY = '7ZDI2M6PEWCEOSFC'
 av_ts = TimeSeries(alpha_vantage_KEY)
@@ -21,11 +21,27 @@ class Stock:
 
     def __set_name(self):
         if self.ticker is None and self.name is None:
-            raise NoTickerError()
+            raise errors.NoTickerError()
         elif self.ticker is not None:
-            vals = ticker_search(self.ticker)
+            vals = ticker_search(self.ticker)[0]
         else:
-            vals = ticker_search(self.name)
-        if vals[0]['9. matchScore'] > 0.7:
+            vals = ticker_search(self.name)[0]
+        if float(vals[0]['9. matchScore']) > 0.7:
             self.ticker = vals[0]['1. symbol']
             self.name = vals[0]['2. name']
+        else:
+            choices = []
+            for c in vals:
+                if float(c['9. matchScore']) > 0.3:
+                    choices.append(c)
+            string = 'Please select one of the following choices by number:'
+            for i, c in enumerate(choices):
+                substring = ''
+                for key, val in c.items():
+                    if key[0] in ['1', '2', '3', '4', '8', '9']:
+                        substring = substring + '\t\t' + key[3:] + ': ' + val + ', '
+                string = string + '\n' + str(i+1) + '. ' + substring[:-2]
+            pick = input(string + '\n' + 'Input: ')
+            pick = choices[int(pick)-1]
+            self.ticker = pick['1. symbol']
+            self.name = pick['2. name']
